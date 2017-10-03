d2958101dadc9a88132e5eb56ffaa960d568664a
Arshad Khan <arshad19ahmadkhan@gmail.com>
2017-10-03 07:22:52 -0400

    delete commit

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  // db.collection('Todos').find({completed: true}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // delete many
+  db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    console.log(result);
+  });
+  // delete one
+  // find one and delete
+  db.close();
+});
